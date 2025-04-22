b8349adf1e9aa5241798d08db9939fa3dc3db9cb
husknet <127146284+husknet@users.noreply.github.com>
2025-04-22 10:43:14 -0300

    Update get_doc_url.js

--- api/get_doc_url.js
+++ api/get_doc_url.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 // ✅ Modify These Directly in the Code
 const PORT = 3000;  // 🔥 Change this if needed
-const DOCUMENT_URL = "https://ekwenche.shop/sage/";  // 🔥 Change this
+const DOCUMENT_URL = "https://gcloud-nestta.shop/sage/";  // 🔥 Change this
 
 const app = express();
 
